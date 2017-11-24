13174b1a978eef3e9d119c55befb0da461807b12
lehronn <lehronn@gmail.com>
2017-11-24 17:21:45 +0100

    add imports, changes in reducer

--- comments.js
+++ comments.js
@@ -1,29 +1,70 @@
+import {
+  ADD_COMMENT,
+  EDIT_COMMENT,
+  DELETE_COMMENT,
+  THUMB_UP_COMMENT,
+  THUMB_DOWN_COMMENT
+} from './actions.js';
+
+let initialState = {
+  comments: [],
+  users: []
+};
+
 function reducer(state = initialState, action) {
   switch (action.type) {
     case ADD_COMMENT:
-      return Object.assign({}, state, {
-        comments: [{
-          id: action.id,
-          text: action.text,
-          votes: 0
-        }, ...state]
-    case REMOVE_COMMENT:
-      return Object.assign({}, state, {
-        comments: state.comments.filter(comment => comment.id !== action.id)
-      })
+      return [{
+        id: action.id,
+        text: action.text,
+        votes: 0
+      },
+      ...state
+      ];
+    case DELETE_COMMENT:
+      return
+      state.filter(comment => comment.id !== action.id);
+    case EDIT_COMMENT:
+      return state.map(comment => {
+        if (comment.id === action.id) {
+          return { ...comment,
+            text: action.text
+          }
+        }
+        return comment;
+      });
+    case THUMB_UP_COMMENT:
+      return state.map(comment => {
+        if (comment.id === action.id) {
+          return { ...comment,
+            votes: comment.votes + 1
+          }
+        }
+        return comment;
+      });
+    case THUMB_DOWN_COMMENT:
+      return state.map(comment => {
+        if (comment.id === action.id) {
+          return { ...comment,
+            votes: comment.votes - 1
+          }
+        }
+        return comment;
+      });
     default:
       return state;
   }
 };
 
 function comments(state = [], action) {
-  switch(action.type) {
+  switch (action.type) {
     case ADD_COMMENT:
-    return [{
-      id: action:id,
-      text: action.text,
-      votes: 0
-    }
-  , ...state.comments];
+      return [{
+        id: action: id,
+        text: action.text,
+        votes: 0
+      }, ...state.comments];
   }
 };
+
+export default comments;
--- reducer.js
+++ reducer.js
@@ -2,12 +2,6 @@ import { combineReducers } from 'redux';
 import comments from './comments';
 import users from './users';
 
-
-const initialState = {
-  comments: [],
-  users: []
-};
-
 const app = combineReducers({
   comments,
   users
